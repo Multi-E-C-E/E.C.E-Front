43770e1b57ec2738e72ef96b1f42711a400934d7
Sebas <sebastiansilvan97@gmail.com>
2023-02-21 11:08:18 -0500

    Estructuracion de carpetas y creacion del componente Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import { React } from 'react';
+import Header from './components/commons/Header';
 
 function App() {
-	const [count, setCount] = useState(0);
-
-	return <h1>Hello word</h1>;
+	return (
+		<>
+			<Header />
+			<div>hola</div>;
+		</>
+	);
 }
 
 export default App;
