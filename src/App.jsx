9d6c6940f426660145dbdc566a12b4cd5d4a97f1
Sebas <sebastiansilvan97@gmail.com>
2023-02-21 22:43:47 -0500

    navegacion del home al dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { React } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/commons/Header';
 import Footer from './components/commons/Footer';
 import Home from './components/Home';
+import  Dashboard  from './components/Dashboard';
 
 function App() {
 	return (
 		<>
 			<Header />
 			<div className='mt-5 appContainer'>
-				<Home />
+				<Routes>
+					<Route path='/' element={<Home />}></Route>
+					<Route path='/dashboar' element={<Dashboard />}></Route>
+				</Routes>
 			</div>
 			<Footer />
 		</>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,18 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
 import { Container } from 'react-bootstrap';
+
+import { Routes, Route } from 'react-router-dom';
 
 const Home = () => {
 	return (
 		<Container>
 			<h5>Hello Home</h5>
+			<ul>
+				<li>
+					<Link to='/dashboar'>aqui habra una imagen</Link>
+				</li>
+			</ul>
 		</Container>
 	);
 };
