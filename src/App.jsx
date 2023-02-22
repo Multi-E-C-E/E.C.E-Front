f83ec9b2482d6faf83aa5d3e1c75be1631ca350e
Sebas <sebastiansilvan97@gmail.com>
2023-02-21 21:55:28 -0500

    Refactorización de codigo ;

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { React } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/commons/Header';
 import Footer from './components/commons/Footer';
-import { Container } from 'react-bootstrap';
+import Home from './components/Home';
 
 function App() {
 	return (
 		<>
 			<Header />
-			<Container className='mt-5 appContainer'>
-				<h5>Hello world</h5>
-			</Container>
+			<div className='mt-5 appContainer'>
+				<Home />
+			</div>
 			<Footer />
 		</>
 	);
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Home = () => {
+	return (
+		<Container>
+			<h5>Hello Home</h5>
+		</Container>
+	);
+};
+
+export default Home;
