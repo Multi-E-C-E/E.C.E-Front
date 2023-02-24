c0c8ce38a2b1a5e3926656f88aa924e476a0abf2
Sebastian Martinez <sebastiansilvan97@gmail.com>
2023-02-24 17:35:22 -0500

    carousel base

--- src/components/homePage/Home.jsx
+++ src/components/homePage/Home.jsx
@@ -1,18 +1,21 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import ControlledCarousel from './Carousel';
 
 const Home = () => {
 	return (
-		<Container>
-			<h5>Hello Home</h5>
-			<ul>
-				<li>
-
-					<Link to='/dashboard'>aqui habra una imagen</Link>
-				</li>
-			</ul>
-		</Container>
+		<>
+			<Container>
+				
+				<ul>
+					<li>
+						<Link to='/dashboard'>aqui habra una imagen</Link>
+					</li>
+				</ul>
+				<ControlledCarousel />
+			</Container>
+		</>
 	);
 };
 
