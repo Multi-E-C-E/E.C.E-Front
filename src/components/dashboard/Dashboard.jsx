2be30b8eb880b2f74e7d04ee31d780b0a4f7f1b7
Sebastian Martinez <sebastiansilvan97@gmail.com>
2023-03-15 15:23:51 -0400

    se actualizao el useFecth a fetchFata

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,30 +1,31 @@
-import React from 'react';
-import { useFecth } from '../../network/useFecth.jsx';
+import React, { Suspense } from 'react';
+import { fetchData } from '../../network/fetchData.jsx';
 import { Card, Button, Container } from 'react-bootstrap';
 import './../../css/dashboard.css';
-
+const api = fetchData('item/all?type=Componente Electronico');
 export const Dashboard = () => {
-	const { items } = useFecth('item/all?type=Componente Electronico');
-
+	const items = api.read();
 	return (
 		<>
 			<Container>
 				<div className='card-container'>
-					{items.map(item => (
-						<Card className='card' key={item.name}>
-							<Card.Img
-								variant='top'
-								className='img-card'
-								src={item.img_item}
-								alt={item.name}
-							/>
-							<Card.Body>
-								<Card.Title>{item.name}</Card.Title>
-								<Card.Text>{item.Type_item.name_type}</Card.Text>
-								<Button variant='primary'>Ver detalles</Button>
-							</Card.Body>
-						</Card>
-					))}
+					<Suspense fallback={<div>Cargando ... </div>}>
+						{items.map(item => (
+							<Card className='card' key={item.name}>
+								<Card.Img
+									variant='top'
+									className='img-card'
+									src={item.img_item}
+									alt={item.name}
+								/>
+								<Card.Body>
+									<Card.Title>{item.name}</Card.Title>
+									<Card.Text>{item.Type_item.name_type}</Card.Text>
+									<Button variant='primary'>Ver detalles</Button>
+								</Card.Body>
+							</Card>
+						))}
+					</Suspense>
 				</div>
 			</Container>
 		</>
---
+++ src/network/fetchData.jsx
@@ -0,0 +1,38 @@
+let BASE_URL = 'http://localhost:3000/api/v1/';
+
+const suspendFun = promise => {
+	let status = 'pending';
+	let response;
+
+	const suspender = promise.then(
+		res => {
+			status = 'success';
+			response = res;
+		},
+		err => {
+			status = 'error';
+			response = err;
+		}
+	);
+
+	const read = () => {
+		switch (status) {
+			case 'pending':
+				throw suspender;
+			case 'error':
+				throw response;
+			default:
+				return response;
+		}
+	};
+
+	return { read };
+};
+
+export function fetchData(END_POINT) {
+	const promise = fetch(BASE_URL + END_POINT)
+		.then(response => response.json())
+		.then(json => json);
+
+	return suspendFun(promise);
+}
