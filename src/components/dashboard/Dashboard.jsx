2702b495e4b547f663bdfdd4bdafce0c37537eac
Sebastian Martinez <sebastiansilvan97@gmail.com>
2023-03-15 14:32:19 -0400

    se agrego el contenido de los items  a una Card

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,19 +1,32 @@
+import React from 'react';
 import { useFecth } from '../../network/useFecth.jsx';
+import { Card, Button, Container } from 'react-bootstrap';
+import './../../css/dashboard.css';
 
 export const Dashboard = () => {
 	const { items } = useFecth('item/all?type=Componente Electronico');
 
 	return (
 		<>
-			<div>
-				{items.map(item => (
-					<li key={item.name}>
-						<img src={item.img_item} alt={item.name} />
-						<h2>{item.name}</h2>
-						<p>{item.Type_item.name_type}</p>
-					</li>
-				))}
-			</div>
+			<Container>
+				<div className='card-container'>
+					{items.map(item => (
+						<Card className='card' key={item.name}>
+							<Card.Img
+								variant='top'
+								className='img-card'
+								src={item.img_item}
+								alt={item.name}
+							/>
+							<Card.Body>
+								<Card.Title>{item.name}</Card.Title>
+								<Card.Text>{item.Type_item.name_type}</Card.Text>
+								<Button variant='primary'>Ver detalles</Button>
+							</Card.Body>
+						</Card>
+					))}
+				</div>
+			</Container>
 		</>
 	);
 };
