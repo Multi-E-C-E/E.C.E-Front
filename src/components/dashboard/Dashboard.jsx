7141cd114d689678d3dce6ff6ce63b0577b0c73c
Sebastian Martinez <sebastiansilvan97@gmail.com>
2023-03-15 16:58:38 -0400

    se agrego la tarjeta para el apartado equipos de medicion

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,18 +1,18 @@
 import React, { Suspense } from 'react';
 import { fetchData } from '../../network/fetchData.jsx';
 import { Card, Button, Container } from 'react-bootstrap';
-import { TbInfoOctagon } from "react-icons/tb";
+import { TbInfoOctagon } from 'react-icons/tb';
 import './../../css/dashboard.css';
-const api = fetchData('item/all');
+const components = fetchData('item/all');
 export const Dashboard = () => {
-	const items = api.read();
+	const items = components.read();
 	return (
 		<>
 			<Container>
-				<div className='card-container'>
+				<div className='dash-card-container'>
 					<Suspense fallback={<div>Cargando ... </div>}>
 						{items.map(item => (
-							<Card className='card' key={item.name}>
+							<Card className='dash-card' key={item.name}>
 								<Card.Img
 									variant='top'
 									className='img-card'
@@ -22,11 +22,32 @@ export const Dashboard = () => {
 								<Card.Body>
 									<Card.Title>{item.name}</Card.Title>
 									<Card.Text>{item.Type_item.name_type}</Card.Text>
-									<TbInfoOctagon className='icon-info'/>
+									<TbInfoOctagon className='icon-info' />
 								</Card.Body>
 							</Card>
 						))}
 					</Suspense>
+				</div>
+
+				<div className='dash-eq-card-container'>
+					<Card className='dash-eq-card'>
+						<Card.Img
+							variant='top'
+							className='img-qe-card'
+							src='https://wallup.net/wp-content/uploads/2014/10/nature/Yellow_Nature_Landscape_Picture.jpg'
+						/>
+						<Card.Body>
+							<Card.Title>{<h1>equiposMedicion</h1>}</Card.Title>
+							<Card.Text>
+								{
+									<h1>
+										Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+										Animi, magnam.
+									</h1>
+								}
+							</Card.Text>
+						</Card.Body>
+					</Card>
 				</div>
 			</Container>
 		</>
