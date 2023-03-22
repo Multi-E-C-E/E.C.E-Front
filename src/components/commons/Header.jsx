343ef3beaed18b5106df5fd2f546f41bc5f3d2fc
Sebastian Martinez <sebastiansilvan97@gmail.com>
2023-03-21 22:17:12 -0400

    implementacion: target : _blank

--- src/components/commons/Header.jsx
+++ src/components/commons/Header.jsx
@@ -8,16 +8,24 @@ export const Header = () => {
 		{ icon: <BsFacebook />, link: 'https://www.facebook.com/upqroo' },
 		{ icon: <BsTwitter />, link: 'https://twitter.com/UPQROO' },
 		{ icon: <BsInstagram />, link: 'https://www.instagram.com/up_qroo/' },
-		{ icon: <BsYoutube />, link: 'https://www.youtube.com/channel/UCTQ6vM_-1DQAVwjvei0ns0Q' },
+		{
+			icon: <BsYoutube />,
+			link: 'https://www.youtube.com/channel/UCTQ6vM_-1DQAVwjvei0ns0Q',
+		},
 	];
 	return (
 		<>
 			<header>
-			<h5 className='h5-hd'>Redes sociales</h5>
+				<h5 className='h5-hd'>Redes sociales</h5>
 				<div className='icon-container'>
-					
 					{socialIcons.map((socialIcon, index) => (
-						<a key={index} href={socialIcon.link} className='hd-icon'>
+						<a
+							key={index}
+							href={socialIcon.link}
+							target='_blank'
+							rel="noopener noreferrer"
+							className='hd-icon'
+						>
 							{React.cloneElement(socialIcon.icon, {
 								style: { height: '30px', width: 'auto' },
 							})}
