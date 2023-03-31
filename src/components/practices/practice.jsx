b7267f6367d2245207e7eade7b04b9d7c377ed3a
glover enrique <gloverotaku@outlook.es>
2023-03-30 23:00:48 -0500

    IMPLEMENTACION DEL ENDPOINT PRACTICAS

--- src/components/practices/practice.jsx
+++ src/components/practices/practice.jsx
@@ -1,19 +1,40 @@
-import { Container } from 'react-bootstrap';
+import { useState } from 'react';
+import { useFecth } from '../../network/useFetch.jsx';
+import { Container, ListGroup, Button } from 'react-bootstrap';
 import './../../css/practices.css';
 
 export const Practice = () => {
-	return (
-		<>
-			<Container>
-					<div className='visualizador-PDF'>
-						<h1>Practicas</h1>
-						<h2>Seleccionar practica a ver</h2>
-						<embed
-							src={URL}
-							type='application/pdf'
-						/>
-					</div>
-			</Container>
-		</>
-	);
+  const { items, loanding, error, detaiError } = useFecth('item/practices');
+  const [selectedPdfUrl, setSelectedPdfUrl] = useState(null);
+  const [showPdf, setShowPdf] = useState(false);
+
+  const handlePdfSelection = (pdfUrl) => {
+    setSelectedPdfUrl(pdfUrl);
+    setShowPdf(true);
+  };
+
+  return (
+    <>
+      <Container>
+        <div className='visualizador-PDF'>
+          <h1>Practicas</h1>
+          <h2>Seleccionar practica a ver</h2>
+          <embed
+            className={`embed-pdf ${showPdf ? 'show' : ''}`}
+            src={selectedPdfUrl}
+            type='application/pdf'
+          />
+          <ListGroup className='practicas-list'>
+            {items.map((items) => (
+              <ListGroup.Item key={items.id_practice}>
+                <Button onClick={() => handlePdfSelection(items.file_url)}>
+                  Practica {items.id_practice}
+                </Button>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </div>
+      </Container>
+    </>
+  );
 };
