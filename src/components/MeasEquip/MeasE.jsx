dc7a1707c28d886308defdd1a4e6ca067a830308
Sebastian Martinez <sebastiansilvan97@gmail.com>
2023-04-04 02:28:17 -0400

    refactor : se reescibio la logica del mapeo, en la lista de imagenes

--- src/components/MeasEquip/MeasE.jsx
+++ src/components/MeasEquip/MeasE.jsx
@@ -4,6 +4,7 @@ import { useFecth, useFecthAwait } from '../../network/useFetch';
 import ToolsList from './ImgList';
 import VideoComponent from './videoComponent';
 import M3D from '../commons/M3D';
+import './../../css/mepd.css';
 
 const MSE = () => {
 	const { items, detaiError, error, loanding, setItems } =
@@ -35,42 +36,20 @@ const MSE = () => {
 
 	return (
 		<>
-			<Container fixed>
-				{error && (
-					<div>
-						<pre>{detaiError.toString()}</pre>
-						<h1>hola</h1>
-					</div>
-				)}
-				{loanding && <div>Cargando ... </div>}
-				<Typography variant='h2' align='center'>
-					{items.name}
-				</Typography>
-				<Grid container alignItems={'center'}>
-					<Grid item xs={12} sm={4} md={6} lg={4} xl={4}>
-						<Box
-							sx={{
-								display: 'flex',
-								alignItems: 'center',
-								justifyContent: 'center',
-							}}
-						>
-							<ToolsList enviar={functionParent} />
-						</Box>
-					</Grid>
-					<Grid item xs={12} sm={8} md={6} lg={8} xl={8}>
-						<Container maxWidth='sm'>
-							<img src={image.url} alt={image.name} style={{ width: '100%' }} />
-						</Container>
-					</Grid>
-				</Grid>
-				<Paper sx={{ p: 2 }}>
-					<h3>Descripcion</h3>
-					<p>{items.description}</p>
-				</Paper>
-				{video.url && <VideoComponent video={video} />}
-				{asset3D.url && <M3D asset3D={asset3D} />}
-			</Container>
+			{error && (
+				<div>
+					<pre>{detaiError.toString()}</pre>
+					<h1>hola</h1>
+				</div>
+			)}
+			{loanding && <div>Cargando ... </div>}
+
+			{items.name}
+			<ToolsList enviar={functionParent} />
+			<img src={image.url} alt={image.name} style={{ width: '100%' }} />
+			<p>{items.description}</p>
+			{video.url && <VideoComponent video={video} />}
+			{asset3D.url && <M3D asset3D={asset3D} />}
 		</>
 	);
 };
