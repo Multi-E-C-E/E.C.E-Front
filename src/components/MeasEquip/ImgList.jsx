40c806f4a25cb405257eba02aa0c3f6db69a8236
Sebastian Martinez <sebastiansilvan97@gmail.com>
2023-04-04 12:19:34 -0400

    estilos para  los intems dentre de las tabs

--- src/components/MeasEquip/ImgList.jsx
+++ src/components/MeasEquip/ImgList.jsx
@@ -1,32 +1,43 @@
 import { Tabs, Tab } from 'react-bootstrap';
 import { useFecth } from '../../network/useFetch';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
 import PropTypes from 'prop-types';
-import './../../css/mepd.css'
 
 const ToolsList = props => {
-	const { items } = useFecth('item/all?type=2');
+	const { items, loanding } = useFecth('item/all?type=2');
 
 	const enviarData = data => {
 		props.enviar(data);
 	};
 
 	return (
-		<Tabs defaultActiveKey='tab1' className='title-tool'>
-			{items.map(image => (
-				<Tab
-					key={image.id_item}
-					eventKey={`tab${image.id_item}`}
-					title={image.name}
-					onClick={() => enviarData(image.id_item)}
-				>
-					<img
-						src={image.preview_url}
-						alt={image.name}
-						style={{ objectFit: 'cover', height: 50 }}
-					/>
-				</Tab>
-			))}
-		</Tabs>
+		<>
+			{' '}
+			<div className='tab-container '>
+				<Tabs defaultActiveKey='tab1' className='title-tool-tab '>
+					{items.map(image => (
+						<Tab
+							key={image.id_item}
+							eventKey={`tab${image.id_item}`}
+							title={image.name}
+							onClick={() => enviarData(image.id_item)}
+						>
+							<div className='tab-items'>
+								<img
+									src={image.preview_url}
+									alt={image.name}
+									className='image-tab'
+								/>
+								<p>
+									{' '}
+									<AiOutlineArrowLeft /> Selecciona la imagen para más detalles{' '}
+								</p>
+							</div>
+						</Tab>
+					))}
+				</Tabs>
+			</div>
+		</>
 	);
 };
 
