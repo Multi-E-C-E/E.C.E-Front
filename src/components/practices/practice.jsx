cd530f9aff7a3b6e408b4e51be5f9138f9f86a13
Luis Angel xool balam <85704920+LangelXoBl@users.noreply.github.com>
2023-04-16 20:32:40 -0500

    Ecf 6 practice module (#41)
    * creacion de componente visor de pdf en practicas
    
    * componentes
    
    * IMPLEMENTACION DEL ENDPOINT PRACTICAS
    
    * modulo practicas funcional
    
    * correcion de import
    
    * view practices refactored
    
    ---------
    
    Co-authored-by: glover enrique <gloverotaku@outlook.es>

--- src/components/practices/practice.jsx
+++ src/components/practices/practice.jsx
@@ -1,6 +1,45 @@
-import { React } from 'react';
+import { useState, useEffect } from 'react';
+import { useFecth } from '../../network/useFetch.jsx';
+import { Container, ListGroup } from 'react-bootstrap';
+import './../../css/Practices.css';
 
-export const  Practice = () => {
-	return <h1>Hello Practices</h1>;
+export const Practice = () => {
+	const { items, loanding, error, detaiError } = useFecth('item/practices');
+	const [selectedPdfUrl, setSelectedPdfUrl] = useState(null);
+
+	const handlePdfSelection = pdfUrl => {
+		setSelectedPdfUrl(pdfUrl + '#zoom=100');
+	};
+
+	useEffect(() => {
+		console.log(items);
+		if (items.length > 0) setSelectedPdfUrl(items[0].file_url);
+	}, [items]);
+
+	return (
+		<>
+			<Container>
+				<div className='text-center'>
+					<h1>Practicas</h1>
+					<h2>Selecciona una parctica para ver</h2>
+				</div>
+				<div className='d-flex'>
+					<ListGroup>
+						{items.slice(0, 3).map(items => (
+							<ListGroup.Item
+								key={items.id_practice}
+								style={{ wordWrap: 'break-word' }}
+								onClick={() => handlePdfSelection(items.file_url)}
+							>
+								Practica {items.id_practice}
+							</ListGroup.Item>
+						))}
+					</ListGroup>
+					<div className='embed.pdf' style={{ width: '80%' }}>
+						<iframe src={selectedPdfUrl}></iframe>
+					</div>
+				</div>
+			</Container>
+		</>
+	);
 };
-
