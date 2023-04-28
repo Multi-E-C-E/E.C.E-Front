6dc4636b455fe97a74f898daa4f2601ff3a919e9
sebastian <sebastiansilvan97@gmail.com>
2023-04-27 22:27:30 -0500

    sobre nosotros: plantilla creada

--- src/components/homePage/us.jsx
+++ src/components/homePage/us.jsx
@@ -10,7 +10,7 @@ export const UsInfo = () => {
 		if (!isClipPathActive) {
 			return <BiHelpCircle onClick={handleClick} className='icon_help' />;
 		} else {
-			return <BiXCircle onClick={handleClick} className='icon_help' />;
+			return <BiXCircle onClick={handleClick} className='icon_help_dark' />;
 		}
 	};
 
@@ -19,10 +19,9 @@ export const UsInfo = () => {
 			{setIconListener()}
 
 			<div className={`main_container ${isClipPathActive ? 'active' : ''}`}>
-
 				<div className='container about_container'>
 					<div className='about_us'>
-						<ul className='list-group-item '>
+						<ul className='list-group-item about_conten_us '>
 							<li className='list-group-item '>An item</li>
 							<li className='list-group-item'>A second item</li>
 							<li className='list-group-item'>A third item</li>
@@ -32,12 +31,22 @@ export const UsInfo = () => {
 					</div>
 
 					<div className='about_page'>
-						<div className="about_conten">
-						<li className='list-group-item '>An item</li>
-						<li className='list-group-item'>A second item</li>
-						<li className='list-group-item'>A third item</li>
-						<li className='list-group-item'>A fourth item</li>
-						<li className='list-group-item'>And a fifth one</li>
+						<div className='about_conten'>
+							<li className='list-group-item '>
+								Bienvenido a la asignatura "Principios de Electrónica
+							</li>
+							<li className='list-group-item'>Aquí encontrarás:</li>
+							<li className='list-group-item'>
+								Información general de los componentes electrónicos de la
+								materia'
+							</li>
+							<li className='list-group-item'>
+								Prácticas de laboratorio para su desarrollo{' '}
+							</li>
+							<li className='list-group-item'>
+								Cuadernillos de ejercicios y hojas de datos, referentes a los
+								componentes electrónicos
+							</li>
 						</div>
 					</div>
 				</div>
