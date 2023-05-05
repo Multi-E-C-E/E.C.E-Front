1439f392e35eccbb73c93f0d70fb1b8314e1e179
sebastian <sebastiansilvan97@gmail.com>
2023-05-04 23:46:09 -0500

    feat: tabs añadidas

--- src/components/componentDetail/ComponentDeets.jsx
+++ src/components/componentDetail/ComponentDeets.jsx
@@ -1,11 +1,12 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { Paper } from '@mui/material';
-import { Container } from 'react-bootstrap';
+import { Tabs, Tab, Container } from 'react-bootstrap';
 import { useFecth } from '../../network/useFetch.jsx';
 import { LateralModal } from './ComponentModals.jsx';
-import { LateralModal3D } from './ComponentModals3D.jsx';
-import '../../css/details.css';
+
+import './styles/details.css';
+import M3D from '../commons/M3D.jsx';
+import { Paper } from '@mui/material';
 const ComponentDetail = () => {
 	const { id } = useParams();
 	const { data, detaiError, error, loanding } = useFecth('item/detail/' + id);
@@ -28,51 +29,59 @@ const ComponentDetail = () => {
 		);
 		return asset ?? {};
 	};
+	const [key, setKey] = useState('whatIs');
 
 	return (
 		<>
-			<Container className='text-center'>
-				{error && <div>{detaiError.toString()}</div>}
-				{loanding && <div>Cargando ... </div>}
-				{typeof data !== 'undefined' && (
-					<>
-						<div className='row'>
-							<h1>{data.name}</h1>
-						</div>
-						<div className='card-container'>
-							<div className='col-2'>
-								<LateralModal
-									data={{
-										haveImg: true,
-										...data.Symbology,
-										title: <h5> Symbología </h5>,
-									}}
-								/>
-								<LateralModal
-									data={{
-										haveImg: false,
-										title: <h5> Aplicaciones </h5>,
-										description: data.aplication,
-									}}
-								/>
-								<LateralModal
-									data={{
-										haveImg: false,
-										title: <h5> ¿Qué es? </h5>,
-										description: data.description,
-									}}
-								/>
-								{asset3D.url && (
-									<LateralModal3D
-										data={{ title: <h5> Modelo 3D </h5>, ...asset3D }}
+			<div>
+				<h1>{data.name}</h1>
+			</div>
+			<Container>
+				<div className='g-container'>
+					{error && <div>{detaiError.toString()}</div>}
+					{loanding && <div>Cargando ... </div>}
+					{typeof data !== 'undefined' && (
+						<>
+							<div className='symbology_container'>
+								<div className='symbology'>
+									<LateralModal
+										data={{
+											haveImg: true,
+											...data.Symbology,
+											title: <p className='modal_title'> Simbología </p>,
+										}}
 									/>
-								)}
+								</div>
 							</div>
-							<img src={image.url} className='img-detail' />
-							<Paper sx={{ p: 2 }}>{data.description}</Paper>
-						</div>
-					</>
-				)}
+
+							<div className='render_3d_container'>
+								<div>
+									<iframe
+										className='render3D'
+										src={`https://sketchfab.com/models/${asset3D.url}/embed`}
+									></iframe>
+								</div>
+							</div>
+						</>
+					)}
+				</div>
+			</Container>
+			<Container>
+				<div className='information_container'>
+					<div className='information '>
+						<Tabs  activeKey={key} onSelect={k => setKey(k)}>
+							<Tab  eventKey='whatIs' title='Que es'>
+								<div className='information'>{data.whatIs}</div>
+							</Tab>
+							<Tab eventKey='description' title='Descripcion'>
+								<div className='information'>{data.description}</div>
+							</Tab>
+							<Tab eventKey='aplication' title='Applicacion'>
+								<div className='information'>{data.aplication}</div>
+							</Tab>
+						</Tabs>
+					</div>
+				</div>
 			</Container>
 		</>
 	);
